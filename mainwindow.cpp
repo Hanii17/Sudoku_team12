eaf49efde8a8574904d722fff0e4aef0213b6b0c
HZY <email@HZY.com>
2019-10-11 20:17:00 +0800

    更新了按钮字体和颜色

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,24 +16,24 @@ MainWindow::MainWindow(QWidget *parent)
         buttonGroup->addButton(findChild<QPushButton*>("btn_"+QString::number(i)),i);
     }
    //设置选择按钮的样式
-    ui->d_1->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_2->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_3->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_4->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_5->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_6->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_7->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_8->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
-    ui->d_9->setStyleSheet("QPushButton{border:1px solid lightgray; background:rgb(236,236,236);}"
-                           "QPushButton:hover{border-color:green; background:transparent;}");
+    ui->d_1->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_2->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_3->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_4->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_5->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_6->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_7->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_8->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
+    ui->d_9->setStyleSheet("QPushButton{border:2px outset lightgray; background:rgb(236,236,236);}"
+                           "QPushButton:hover{border-color:lightgray; background:transparent;}");
    //设置九宫格样式
     for(int i=1;i<=81;i++)
     {
@@ -43,69 +43,69 @@ MainWindow::MainWindow(QWidget *parent)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-right:2px outset black;border-left:1px outset lightgray;"
                                                   " border-top:2px outset black;border-bottom:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置左上边框
         else if(i==1||i==28||i==55||i==4||i==31||i==58||i==7||i==34||i==61)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-left:2px outset black;border-right:1px outset lightgray;"
                                                   " border-top:2px outset black;border-bottom:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置左下边框
         else if(i==19||i==46||i==73||i==22||i==49||i==76||i==25||i==52||i==79)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-left:2px outset black;border-right:1px outset lightgray;"
                                                   " border-bottom:2px outset black;border-top:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置右下边框
         else if(i==21||i==24||i==27||i==48||i==51||i==54||i==75||i==78||i==81)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-right:2px outset black;border-left:1px outset lightgray;"
                                                   " border-bottom:2px outset black;border-top:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置上边框
         else if(i==2||i==5||i==8||i==29||i==32||i==35||i==59||i==56||i==62)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-top:2px outset black;border-left:1px outset lightgray;"
                                                   " border-right:1px outset lightgray;border-bottom:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置左边框
         else if(i==10||i==13||i==16||i==37||i==40||i==43||i==64||i==67||i==70)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-left:2px outset black;border-right:1px outset lightgray;"
                                                   " border-top:1px outset lightgray;border-bottom:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         //设置下边框
         else if(i==74||i==77||i==80||i==47||i==50||i==53||i==20||i==23||i==26)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-bottom:2px outset black;border-right:1px outset lightgray;"
                                                   " border-top:1px outset lightgray;border-left:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
        //设置右边框
         else if(i==18||i==45||i==72||i==12||i==39||i==66||i==42||i==15||i==69)
         {
             buttonGroup->button(i)->setStyleSheet("QPushButton{border-right:2px outset black;border-bottom:1px outset lightgray;"
                                                   " border-top:1px outset lightgray;border-left:1px outset lightgray;background:rgb(236,236,236);}"
-                                                  "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                                  "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");
+                                                  "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                                  "QPushButton:checked{background-color:rgb(195, 222, 244);}");
         }
         else{
         buttonGroup->button(i)->setStyleSheet("QPushButton{border:1px outset lightgray; background:rgb(235,235,235);}"
-                                              "QPushButton:hover{border-color:blue; background:rgb(228 , 240 , 250);}"
-                                              "QPushButton:checked{background-color:rgb(228 , 240 , 250);}");}
+                                              "QPushButton:hover{border-color:blue; background:rgb(195, 222, 244);}"
+                                              "QPushButton:checked{background-color:rgb(195, 222, 244);}");}
     }
 
 
